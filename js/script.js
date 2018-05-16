4249ec2b1b301a960bf334d6396d0021fb8e8bdb
Luis Falcon <luisdark935@hotmail.com>
2018-05-16 15:36:11 -0500

    Optimizacion del Script script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ window.onload = function(){
 loading.style.visibility = 'hidden';
 loading.style.opacity = '0';
 }
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5afa4d8a5f7cdf4f053439b1/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
