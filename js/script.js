1f363fbda08a1e7e3ef44d3b12a30a4bcbcae4ae
Luis Miguel Falcon Montaño <luisdark935@hotmail.com>
2018-06-22 18:35:24 -0500

    Actualización de la imagen de perfil

--- js/script.js
+++ js/script.js
@@ -11,12 +11,12 @@ $boton.addEventListener('click', function(){
 	$boton.classList.toggle('icon-minus');
 });
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/5afa4d8a5f7cdf4f053439b1/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
+
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '488e7c4775dde4ab338188dc198f3d0cfefb7b49';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
