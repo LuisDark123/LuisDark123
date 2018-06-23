e20560f2f301fb9e97f29d0d7aaeeba6de184a3d
Luis Miguel Falcon Montaño <luisdark935@hotmail.com>
2018-06-22 22:16:00 -0500

    Mejora de los artículos del blog

--- js/script.js
+++ js/script.js
@@ -10,13 +10,3 @@ $boton.addEventListener('click', function(){
 	$menu.classList.toggle('active');
 	$boton.classList.toggle('icon-minus');
 });
-
-
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '488e7c4775dde4ab338188dc198f3d0cfefb7b49';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
